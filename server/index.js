ab6c9e40190874a689b582cdeb105ac421173007
Rahul Patel <rpatel@brightree.com>
2022-03-11 17:25:51 +0530

    mongoose installed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
+
+const keys = require("./config/dev");
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
