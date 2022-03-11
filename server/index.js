04c1196b81d4bf24a6e379d46aa8c9780502e52f
Rahul Patel <rpatel@brightree.com>
2022-03-11 15:50:53 +0530

    google Auth token added

--- server/index.js
+++ server/index.js
@@ -1,10 +1,40 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require("./config/dev");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClinetID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refereshToken, profile, done) => {
+      console.log("access Token : ", accessToken);
+      console.log("referesh Token :", refereshToken);
+      console.log("profile :", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"), async (req, res) => {
+  console.log(JSON.stringify(req.user));
+  
 });
 
+// app.get("/", (req, res) => {
+//   res.send({ bye: "buddy" });
+// });
+
 const PORT = process.env.PORT || 5000;
- 
+
 app.listen(PORT);
