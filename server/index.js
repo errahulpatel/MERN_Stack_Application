9a6b5de8ea08f38ae0ea5914971ec318da4400de
Rahul Patel <rpatel@brightree.com>
2022-03-14 16:56:44 +0530

    passport cookie changes

--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require("./services/passport");
-const authRoutes = require("./routes/authRoutes");
-
-const keys = require("./config/dev");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-authRoutes(app);
-//require('./routes/authRoutes')(app);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send({ Hi: "buddy" });
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
