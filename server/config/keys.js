2cb000dbc288f68ac1b7a57abfd4bec2aeb46e99
Rahul Patel <rpatel@brightree.com>
2022-03-14 17:20:24 +0530

    chnage keys

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+//keys.js - figure out what set of Credentials to return
+if (ProcessingInstruction.env.NODE_ENV === "production") {
+  // We are in Production
+  module.exports = require("./prod");
+} else {
+  //we ar ein Development
+  module.exports = require("./dev");
+}
