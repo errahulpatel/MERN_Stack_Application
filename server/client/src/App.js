ff344ff05c9274c3ef5a1be693fd48bd266d7b44
Rahul Patel <rpatel@brightree.com>
2022-03-15 14:35:57 +0530

    setup proxy in client

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h2>Hi There</h2>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <a  className="App-link" href="/auth/google">Sign In With Google</a>
       </header>
     </div>
   );
