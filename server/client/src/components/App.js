6b6bb9ebeb5342322b76719290ccf04b51eb8ada
Rahul Patel <rpatel@brightree.com>
2022-03-15 17:13:39 +0530

    reducer setup and Routes added

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./Header";
+import Landing from "./Landing";
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
 
 const App = () => {
-  return (<div>Hi There!</div>);
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import App from "./components/App";
+import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 
-ReactDom.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
+ReactDOM.render(
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root')
 );
