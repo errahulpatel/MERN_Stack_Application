8fcf919bcf3d4118188417fb3a509095b78725e2
Rahul Patel <rpatel@brightree.com>
2022-03-28 18:56:30 +0530

    material icon added

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   return (
-    <h2>Dashboard</h2>
+    <div>
+      Dashboard
+      <div className="fixed-action-btn">
+        <Link to="/surveys/new" className="btn-floating btn-large red">
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    </div>
   );
 };
 
